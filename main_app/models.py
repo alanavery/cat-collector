b74a2f5cce74710506de9243395679dec6d76274
delayedaa <delayedaa@gmail.com>
2021-01-14 15:35:16 -0600

    Add CatToy model

--- main_app/models.py
+++ main_app/models.py
@@ -4,6 +4,14 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 
+class CatToy(models.Model):
+    name = models.CharField(max_length=100)
+    color = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
 class Cat(models.Model):
     name = models.CharField(max_length=100)
     breed = models.CharField(max_length=100)
