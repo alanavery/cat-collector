2741debe903ee35893ad92e7930404de0bb1af76
delayedaa <delayedaa@gmail.com>
2021-01-14 11:46:48 -0600

    Add Cat model and admin account

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
+from .models import Cat
 
 # Create your views here.
+
+
+# class Cat:
+#     def __init__(self, name, breed, description, age):
+#         self.name = name
+#         self.breed = breed
+#         self.description = description
+#         self.age = age
+
+
+# cats = [
+#     Cat('Lolo', 'tabby', 'foul little demon', 3),
+#     Cat('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
+#     Cat('Raven', 'black tripod', '3 legged cat', 4)
+# ]
 
 
 def index(request):
@@ -9,3 +25,8 @@ def index(request):
 
 def about(request):
     return render(request, 'about.html')
+
+
+def cats_index(request):
+    cats = Cat.objects.all()
+    return render(request, 'cats/index.html', {'cats': cats})
