4b7070fdffcc1ec3c44e33ca81add1412c58779a
delayedaa <delayedaa@gmail.com>
2021-01-16 15:38:53 -0600

    Update SECRET_KEY

--- catcollector/settings.py
+++ catcollector/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
+from dotenv import load_dotenv
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -19,8 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
+# Load dotenv for storing SECRET_KEY
+load_dotenv()
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'z^s@_m1b-c&)s6=lrg*u6*)lo8^qvf90j79_ct20=trw@ut9k%'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
